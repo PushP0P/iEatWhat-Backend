a7a74c802a501b797942192e10d945553d9993f5
Raven <rdev1163@outlook.com>
2018-02-03 08:42:46 -0800

    built models sub-project and working gradle build :)

--- src/main/java/PublicAPI.java
+++ src/main/java/PublicAPI.java
@@ -1,38 +1,47 @@
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import ratpack.http.MutableHeaders;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static ratpack.jackson.Jackson.json;
-
 public class PublicAPI {
 
-    public static class ArticleContent {
-        private String title;
-        private String blurb;
+    public Map<String, Handler> postRequestHandlers() {
+        Map<String, Handler> handlers = new HashMap<>();
 
-        public ArticleContent() {
-            this.title = "Testing JSON";
-            this.blurb = "Successful JSON Transport and Parse";
-        }
+        return handlers;
     }
 
-    public Map<String, Server.GetHandler> getRequestHandlers() {
-        Map<String, Server.GetHandler> handlers = new HashMap<>();
+    public Map<String, Handler> getRequestHandlers() {
+        Map<String, Handler> handlers = new HashMap<>();
         handlers.put("", (ctx) -> ctx.render("FOO BAT METAL"));
         handlers.put("public", (ctx) -> ctx.render("Public API Dynamic Success!"));
         handlers.put(
-            "json",
+            "demo",
             (ctx) -> {
-                final JsonNodeFactory jsonNodeFactory = JsonNodeFactory.instance;
-                ObjectNode articleJSON = jsonNodeFactory.objectNode();
-                ArticleContent articleContent = new ArticleContent();
-                articleJSON.put("title", articleContent.title);
-                articleJSON.put("blurb", articleContent.blurb);
-                ctx.render(json(articleJSON));
+                ObjectMapper om = new ObjectMapper();
+//                final JsonNodeFactory jsonNodeFactory = JsonNodeFactory.instance;
+//                ObjectNode articleJSON = jsonNodeFactory.objectNode();
+                Article article = dummyContent();
+                String articleAsString = om.writeValueAsString(article);
+                ctx.render(articleAsString);
             });
         return handlers;
     }
+
+    private void eventHandler(Context ctx) {
+
+    }
+
+    public static Article dummyContent() {
+        ArticleSection[] sections;
+        sections = new ArticleSection[3];
+        String paragraph = "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit\\u200B\\u200B, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead. Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit\\u200B\\u200B, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.";
+        sections[0] = new ArticleSection("Zombie", paragraph);
+        sections[1] = new ArticleSection("Zombies", paragraph);
+        sections[2] = new ArticleSection("ZOMBIEZ!!", paragraph);
+        return new Article("1235ASD", "Zombie Ipsum", "Brains!!!!", sections);
+    }
 }
+
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,6 +1,5 @@
-import ratpack.handling.Context;
+import ratpack.handling.Handler;
 import ratpack.http.MutableHeaders;
-import ratpack.http.Response;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 
@@ -8,24 +7,23 @@ import java.util.Map;
 
 public class Server {
 
-    public interface GetHandler {
-        void getHandler(Context ctx);
-    }
-
-    public void startServer(Map<String, GetHandler> handlers) throws Exception {
+    public void startServer(Map<String, Handler> handlers) throws Exception {
         RatpackServer.start(server -> server
         .serverConfig(config -> config
+        .onError(System.out::println)
         .baseDir(BaseDir.find())
         .env())
         .handlers(chain -> {
-            for (Map.Entry<String, GetHandler> handler: handlers.entrySet()) {
+            for (Map.Entry<String, Handler> handler: handlers.entrySet()) {
                 chain.get(handler.getKey(), ctx -> {
                     MutableHeaders headers = ctx.getResponse().getHeaders();
                     headers.set("Access-Control-Allow-Origin", "*");
                     headers.set("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
-                    handler.getValue().getHandler(ctx);
+                    handler.getValue().handle(ctx);
                 });
             }
         }));
     }
+
 }
+
