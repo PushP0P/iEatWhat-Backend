82506ec2d147dc94ea767a135de29eae07da5db8
Raven <rdev1163@outlook.com>
2018-02-06 14:28:52 -0800

    client deploy tasks built and integrated for heroku

--- src/api/src/main/java/PublicAPI.java
+++ src/api/src/main/java/PublicAPI.java
@@ -1,21 +1,13 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ratpack.handling.Handler;
-//import ratpack.handling.Context;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class PublicAPI {
 
-//    public Map<String, Handler> postRequestHandlers() {
-//        Map<String, Handler> handlers = new HashMap<>();
-//
-//        return handlers;
-//    }
-
     public Map<String, Handler> getRequestHandlers() {
+
         Map<String, Handler> handlers = new HashMap<>();
-        handlers.put("", (ctx) -> ctx.render("FOO BAT METAL"));
         handlers.put("public", (ctx) -> ctx.render("Public API Dynamic Success!"));
         handlers.put(
             "demo",
@@ -24,14 +16,7 @@ public class PublicAPI {
                 Article article = Article.demoData();
                 String articleAsString = om.writeValueAsString(article);
                 ctx.render(articleAsString);
-            });
+        });
         return handlers;
     }
-
-//    private void eventHandler(Context ctx) {
-//
-//    }
-
-
 }
-
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,3 +1,5 @@
+import ratpack.groovy.template.TextTemplateModule;
+import ratpack.guice.Guice;
 import ratpack.handling.Handler;
 import ratpack.http.MutableHeaders;
 import ratpack.server.BaseDir;
@@ -5,24 +7,32 @@ import ratpack.server.RatpackServer;
 
 import java.util.Map;
 
+import static ratpack.groovy.Groovy.groovyTemplate;
+
 public class Server {
 
     public void startServer(Map<String, Handler> handlers) throws Exception {
         RatpackServer.start(server -> server
-        .serverConfig(config -> config
-        .onError(System.out::println)
-        .baseDir(BaseDir.find())
-        .env())
-        .handlers(chain -> {
-            for (Map.Entry<String, Handler> handler: handlers.entrySet()) {
-                chain.get(handler.getKey(), ctx -> {
-                    MutableHeaders headers = ctx.getResponse().getHeaders();
-                    headers.set("Access-Control-Allow-Origin", "*");
-                    headers.set("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
-                    handler.getValue().handle(ctx);
-
-                });
-            }
-        }));
+            .serverConfig(config -> config
+            .onError(System.out::println)
+            .baseDir(BaseDir.find())
+            .env())
+            .registry(Guice.registry(b -> b.module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true))))
+            .handlers(chain -> {
+                for (Map.Entry<String, Handler> handler: handlers.entrySet()) {
+                    chain.prefix("api", api -> {
+                        api.path(handler.getKey(), ctx -> {
+                            MutableHeaders headers = ctx.getResponse().getHeaders();
+                            headers.set("Access-Control-Allow-Origin", "*");
+                            headers.set("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+                            handler.getValue().handle(ctx);
+                        });
+                    });
+                }
+                chain.get("", ctx -> ctx.render(groovyTemplate( "index.html")));
+                chain.get(":view", ctx -> ctx.render(groovyTemplate( "index.html")));
+                chain.files(f -> f.dir("public"));
+            })
+        );
     }
 }
