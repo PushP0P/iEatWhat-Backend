9e5adad0ccb562aceeec3547e92da0dac45351cc
Raven <rdev1163@outlook.com>
2018-02-07 13:30:46 -0800

    refactoring the gradle.build for frontend client

--- src/client/src/router/router.history.tsx
+++
@@ -1,6 +0,0 @@
-import { createBrowserHistory } from 'history';
-
-export default createBrowserHistory({
-	basename: '/',
-	forceRefresh: true
-});
--- src/client/src/services/sign-in.service.ts
+++
@@ -1,5 +0,0 @@
-import { FIXTURE_SIGN_IN_OPTIONS } from '../models/sign-in.model';
-
-export async function getSignInOptions() {
-	return Promise.resolve(FIXTURE_SIGN_IN_OPTIONS);
-}
