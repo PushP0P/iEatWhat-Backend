ddd9d1cc90cc2f470be3016d663e7d4a05f8d873
Raven <rdev1163@outlook.com>
2018-02-24 10:34:35 -0800

    updated client

--- src/search-manager/src/main/java/SearchManager/FoodSearch.java
+++ src/search-manager/src/main/java/SearchManager/FoodSearch.java
@@ -1,18 +1,27 @@
 package SearchManager;
 
 import DBManager.DBManager;
+import Models.Model;
 import Models.Review;
+
+import java.util.List;
 
 public class FoodSearch {
 
     public FoodSearch() {
+
     }
 
     public static void searchFood() throws Exception {
-        Review review = new Review("Rock", "Bar", "Bas", "Bat", "Metal");
-        DBManager.insert(review);
-        review.setAuthor_id("New Author");
-        DBManager.insert(review);
+//        List<Model> reviews = DBManager.find(new Review());
+//        for (Model i: reviews) {
+//            System.out.println(i.());
+//        }
+//
+//        Review review = new Review("Rock", "Bar", "Bas", "Bat", "Metal");
+//        DBManager.insert(review);
+//        review.setAuthor_id("New Author");
+//        DBManager.insert(review);
     }
 
 }
